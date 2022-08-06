a2011d5361692c7c1163b8100e2ff4f30a0dc17f
Rafael Villalobos <rvilla3452@gmail.com>
2022-08-05 22:17:10 -0600

    build

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-const whitelist = ['http://localhost:8080', 'http://192.168.100.5:8080'];
+const whitelist = ['http://localhost:8080', 'https://pizzailcapo.com/', 'http://192.168.100.5:8080'];
 const options = {
   origin: (origin, callback) => {
     if (whitelist.includes(origin) || !origin) {
